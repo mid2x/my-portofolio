3b998a5f9263b5647cddc498e69c16151534c33f
Hamid Shebubakar <hamid.shebubakar@gmail.com>
2017-09-21 16:18:49 +0700

    add try and exception with SNS

--- lambdaonaws.py
+++ lambdaonaws.py
@@ -3,20 +3,28 @@ import StringIO
 import zipfile
 import mimetypes
 def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic("arn:aws:sns:us-east-1:980307711485:snsPublishDeploy")
+    try:
+        s3 = boto3.resource('s3')
+        porto = s3.Bucket('baalwy.com')
+        build = s3.Bucket('codebuild.baalwy.com')
+    
+        porto_zip = StringIO.StringIO()
+        build.download_fileobj('protofoliobuild.zip', porto_zip)
+    
+        with zipfile.ZipFile(porto_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                porto.upload_fileobj(obj, nm, 
+                    ExtraArgs={'ContentType' : mimetypes.guess_type(nm)[0]})
+                porto.Object(nm).Acl().put(ACL='public-read')
+        
+        topic.publish(Subject = "Portofolio Deployed", Message= "This is from Try Block")
+    except:
+        topic.publish(Subject = "Portofolio Deployed Failed", Message= "This is from exception Block")
+        raise
 
-    s3 = boto3.resource('s3')
-    porto = s3.Bucket('baalwy.com')
-    build = s3.Bucket('codebuild.baalwy.com')
-
-    porto_zip = StringIO.StringIO()
-    build.download_fileobj('protofoliobuild.zip', porto_zip)
-
-    with zipfile.ZipFile(porto_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            porto.upload_fileobj(obj, nm, 
-            	ExtraArgs={'ContentType' : mimetypes.guess_type(nm)[0]})
-            porto.Object(nm).Acl().put(ACL='public-read')
-    print "job done!"
     return 'Hello from Lambda'
+    
     
